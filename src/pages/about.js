c6d9b734f89efcf84da44bf2d3dba3a30c8cc00c
martin starosta <labzone@bait.sk>
2020-10-08 10:42:46 +0200

    First commit.

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/header'
+export default function About () {
+  return (
+    <div style={{ color: `teal` }}>
+     <Header headerText='This is about page' />
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import { Link } from 'gatsby'
+
+import Header from '../components/header'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to='/contact'>Contact us</Link>
+      <Header headerText='Home page'/>
+      <p>What a beautiful world.</p>
+
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
