102a7f1bab619a7fe10c58bf14d0b9048fc361df
Martin Starosta <martin.starosta83@gmail.com>
2020-11-15 23:59:29 +0100

    Moves script to head

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,13 +3,15 @@ export const onRenderBody = (
   { setHeadComponents, setPostBodyComponents },
   pluginOptions
 ) => {
-  setPostBodyComponents([
+    setHeadComponents([
     <script
       src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
       type="text/javascript"
       charset="UTF-8"
       data-domain-script="833401ed-6965-4b06-a80b-9282226ea246"
     ></script>,
-    <script type="text/javascript">function OptanonWrapper() {}</script>,
+    <script type="text/javascript">
+        function OptanonWrapper() {}
+    </script>,
   ])
 }
