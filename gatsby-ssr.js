94cb10d8052d669c0335f305a8bdf4cf4b2ee5c9
Martin Starosta <martin.starosta83@gmail.com>
2020-11-20 22:58:22 +0100

    Adds title and description to pages

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,7 +7,7 @@ export const onRenderBody = (
     <script
       src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
       type="text/javascript"
-      charset="UTF-8"
+      charSet="UTF-8"
       data-domain-script="833401ed-6965-4b06-a80b-9282226ea246"
     ></script>,
     <script type="text/javascript">
