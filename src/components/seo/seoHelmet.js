fad0aab9e9c9d0b07fc474de5097ed3ec8e393a6
Martin Starosta <martin.starosta83@gmail.com>
2020-11-20 23:11:36 +0100

    Adds lang attribute to html tag

--- src/components/seo/seoHelmet.js
+++ src/components/seo/seoHelmet.js
@@ -2,10 +2,10 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 export default function SeoHelmet(props) {
-  const { title, description, canonical } = props
+  const { title, description, canonical, lang = 'en'} = props
   return (
     <>
-      <Helmet>
+      <Helmet htmlAttributes={{ lang }}>
         <meta charSet="utf-8" />
         <title>{`${title} | LabZone`}</title>
         {description && <meta name='description' content={description} />}
