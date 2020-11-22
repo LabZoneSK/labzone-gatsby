0261c5ecd80310a68f609a1f9e3ace0cd1288129
Martin Starosta <martin.starosta83@gmail.com>
2020-11-22 21:56:52 +0100

    Generates robots.txt based on env.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@
  */
 require("dotenv").config()
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://www.labzone.tech",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === "production"
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
 const path = require("path")
 
 module.exports = {
@@ -82,7 +90,30 @@ module.exports = {
         },
       },
     },
+    /* SEO */
     `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
+    /* Analytics */
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
