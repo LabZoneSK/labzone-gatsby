6c26740cfe297e590f70a2c3153e227bb8fdd94a
Martin Starosta <martin.starosta83@gmail.com>
2021-01-24 17:43:54 +0100

    Move Hubspot functions to helpers, getCookie function added.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,3 +10,20 @@ export function sanitizeLink(path, langKey){
   }
   return path.replace(`/${defaultLangKey}/`, '/');
 }
+
+/** Function gets cookie by specified name */
+export function getCookie(cname) {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
