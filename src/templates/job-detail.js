936c1c1f0cd2f68f9f2473ad16e2ae3fa578c865
Martin Starosta <martin.starosta83@gmail.com>
2021-09-25 20:37:35 +0200

    Adds JSON-LD to Job offer

--- src/templates/job-detail.js
+++ src/templates/job-detail.js
@@ -6,12 +6,10 @@ import { FormattedMessage } from "react-intl"
 
 /** Components */
 import Layout from "../components/layout"
-import ContactUs from "../components/contactUs"
 import FullBleed from "../components/fullBleed"
 import Hero from "../components/hero"
-import Img from "gatsby-image"
-import Section from "../components/section"
 import SeoHelmet from "../components/seo/seoHelmet"
+import JsonLD from "../components/seo/JsonLd"
 
 /** Prismic */
 import { RichText } from "prismic-reactjs"
@@ -23,7 +21,7 @@ import { sanitizeLink } from "../utils/helpers"
 export default function JobDetail({ data, location }) {
   const {
     title,
-    allocation,
+    basesalary,
     description,
     responsibilities,
     requirements,
@@ -32,10 +30,41 @@ export default function JobDetail({ data, location }) {
   } = data.prismicJoboffer.data
 
   const lang = data.prismicJoboffer.lang
+  const metadata = {
+    "@context": "https://schema.org/",
+    "@type": "JobPosting",
+    title: title.text,
+    description:description.text,
+    identifier: {
+      "@type": "PropertyValue",
+      name: "LabZone",
+      value: "Job-" + title.text,
+    },
+    datePosted: "2017-01-18",
+    validThrough: "2017-03-18T00:00",
+    jobLocationType: "TELECOMMUTE",
+    employmentType: "CONTRACT",
+    hiringOrganization: {
+      "@type": "Organization",
+      name: "LabZone",
+      sameAs: "https://labzone.tech",
+      logo: "https://www.labzone.tech/images/logo-square-red.png",
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: "EUR",
+      value: {
+        "@type": "QuantitativeValue",
+        value: parseFloat(basesalary),
+        unitText: "MONTH",
+      },
+    },
+  }
 
   return (
     <>
       <SeoHelmet title={`${title.text}`} lang={lang} />
+      <JsonLD>{metadata}</JsonLD>
 
       <Layout location={location}>
         <FullBleed>
@@ -48,21 +77,40 @@ export default function JobDetail({ data, location }) {
           </h1>
           <RichText render={description.raw} linkResolver={linkResolver} />
 
-          <h2><FormattedMessage id="whatInvolves" defaultMessage="What it involves" /></h2>
+          <h2>
+            <FormattedMessage
+              id="whatInvolves"
+              defaultMessage="What it involves"
+            />
+          </h2>
           <RichText render={responsibilities.raw} linkResolver={linkResolver} />
 
-          <h2><FormattedMessage id="lookingForSkills" defaultMessage="Skills we´re looking for" /></h2>
+          <h2>
+            <FormattedMessage
+              id="lookingForSkills"
+              defaultMessage="Skills we´re looking for"
+            />
+          </h2>
           <RichText render={requirements.raw} linkResolver={linkResolver} />
 
-          <h2><FormattedMessage id="delightedToHave" defaultMessage="Delighted if you have" /></h2>
+          <h2>
+            <FormattedMessage
+              id="delightedToHave"
+              defaultMessage="Delighted if you have"
+            />
+          </h2>
           <RichText render={nicetohave.raw} linkResolver={linkResolver} />
 
-          <h2><FormattedMessage id="whatOffer" defaultMessage="What we offer" /></h2>
+          <h2>
+            <FormattedMessage id="whatOffer" defaultMessage="What we offer" />
+          </h2>
           <RichText render={offer.raw} linkResolver={linkResolver} />
 
           <p className="is-size-5 mb-5">
-          <FormattedMessage id="jobClosingText" defaultMessage="There's a lot more to us than meets the eye. So if this resonates with you, we´d like to hear from you." />
-            
+            <FormattedMessage
+              id="jobClosingText"
+              defaultMessage="There's a lot more to us than meets the eye. So if this resonates with you, we´d like to hear from you."
+            />
           </p>
 
           <div className="mt-6 center">
@@ -85,16 +133,15 @@ export const query = graphql`
       uid
       lang
       data {
+        basesalary
         title {
-          text
-        }
-        allocation {
           text
         }
         location {
           text
         }
         description {
+          text
           raw
         }
         responsibilities {
