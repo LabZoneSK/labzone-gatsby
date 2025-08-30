c63d440e7ab971448d7527f31353377f7d4e150b
Martin Starosta <martin.starosta@labzone.tech>
2025-08-30 13:37:38 +0200

    Tailwind v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        './src/**/*.{js,jsx,ts,tsx}',
         './src/pages/**/*.{js,jsx,ts,tsx}',
         './src/components/**/*.{js,jsx,ts,tsx}',
         './src/templates/**/*.{js,jsx,ts,tsx}',
         './src/common/**/*.{js,jsx,ts,tsx}',
     ],
+    important: true, // This makes all Tailwind classes important
     theme: {
         extend: {
             colors: {
